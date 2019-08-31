8e615968ff6a9a2d8220d9cc8b6bce30e2a236e7
Yordi Ogando <yordiogando@gmail.com>
2019-08-31 15:08:29 -0400

    Models Update

--- main/models.py
+++ main/models.py
@@ -10,14 +10,24 @@ class College (models.Model):
     adress = models.CharField(max_length = 100)
     phone_number = models.CharField(max_length = 10)
 
+    def __str__(self):
+        return self.name
+
 
 class Subject (models.Model):
     name = models.CharField(max_length = 50)
     area = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return self.name
+
 class User (AbstractUser):
     is_student = models.BooleanField(default = False)
     is_teacher = models.BooleanField(default = False)
+
+    def __str__(self):
+        return '%s, %s' % ( self.first_name, self.last_name)
+
 
 class Student (models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, default = 1, primary_key = True)
@@ -35,9 +45,19 @@ class TutorD (models.Model):
 
 
 class User_Request (models.Model):
-     student_ID = models.ForeignKey(Student, on_delete = models.CASCADE)
-     tutor_id = models.ForeignKey(TutorH,  on_delete = models.CASCADE)
-     status = models.IntegerField()
-     request_date = models.DateTimeField("Request date", default = datetime.now())
+     
+    REQUEST_STATUS = (
+        ('A', 'Accepted'),
+        ('R', 'Rejected'),
+        ('C', 'Cancelled'),
+        ('P', 'Pending'),
+        )
+
+    student_ID = models.ForeignKey(Student, on_delete = models.CASCADE)
+    tutor_id = models.ForeignKey(TutorH,  on_delete = models.CASCADE)
+    status = models.CharField(max_length = 1, choices = REQUEST_STATUS, blank = True, default = 'P')
+    request_date = models.DateTimeField("Request date", default = datetime.now())
+
+    
 
 

309d7a733f3550e2657e080e6a91e3e454888b87
Yordi Ogando <yordiogando@gmail.com>
2019-08-31 02:41:23 -0400

    Models Update

--- main/models.py
+++ main/models.py
@@ -1,30 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from datetime import datetime
+
 
 # Create your models here.
 
 class College (models.Model):
-    name = models.CharField(max_length = 100)
-    adress = models.CharField(max_length = 150)
+    name = models.CharField(max_length = 50)
+    adress = models.CharField(max_length = 100)
     phone_number = models.CharField(max_length = 10)
 
 
-# class Student (models.Model):
-#     name = models.CharField(max_length = 100)
-#     email = models.CharField(max_length = 100)
-#     password = 
+class Subject (models.Model):
+    name = models.CharField(max_length = 50)
+    area = models.CharField(max_length = 50)
+
+class Student (AbstractUser):
+    college_id = models.ForeignKey(College, on_delete = models.SET_NULL)
+
+class TutorH (AbstractUser):
+    degree = models.CharField(max_length = 50)
+    identity_document = models.CharField(max_length = 11)
+    description = models.CharField(max_length = 100)
+
+class TutorD (models.Model):
+    tutorH_ID = models.OneToOneField(TutorH, on_delete = models.CASCADE)
+    subject_ID = models.ForeignKey(Subject, on_delete = models.SET_NULL)   
 
 
 class User_Request (models.Model):
 
-     student_ID = models.ForeignKey(User, default = 1, on_delete = models.SET_DEFAULT)
-     #tutor_id = models.ForeignKey(,default = 0, on_delete = models.SET_DEFAULT)
+     student_ID = models.ForeignKey(Student, on_delete = models.SET_NULL)
+     tutor_id = models.ForeignKey(TutorH, on_delete = models.SET_NULL)
      status = models.IntegerField()
      request_date = models.DateTimeField("Request date", default = datetime.now())
 
 
-class Subject (models.Model):
-
-    name = models.CharField(max_length = 50)
-    area = models.CharField(max_length = 50)

f07d1cbe863d12e7d3c6bdf608b3a32af6303271
Yordi Ogando <yordiogando@gmail.com>
2019-09-06 14:35:23 -0400

    Mixed Forms

--- main/forms.py
+++ main/forms.py
@@ -2,26 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User, Tutor
 
-class NewUserForm (UserCreationForm):
+class UserSignUp_Form (UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
 
-    def save(self, commit = True):
-        user = super(NewUserForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+    # def save(self, commit = True):
+    #     user = super(UserSignUp_Form, self).save(commit=False)
+    #     user.email = self.cleaned_data['email']
+    #     if commit:
+    #         user.save()
+    #     return user
 
-# class StudentSignUpForm(UserCreationForm):
-#     interests = forms.ModelMultipleChoiceField(
-#         queryset=Subject.objects.all(),
-#         widget=forms.CheckboxSelectMultiple,
-#         required=True
-#     )
-
-#     class Meta(UserCreationForm.Meta):
-#         model = User
+class StudentSignUp_Form(forms.ModelForm):
+    
+    class Meta:
+        model = Tutor
+        fields = ("degree","identity_document","description","subjects")
--- main/views.py
+++ main/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import NewUserForm
+from .forms import UserSignUp_Form, StudentSignUp_Form
 
 # Create your views here.
 
 def homepage(request):
-    form = NewUserForm
+    user_form = UserSignUp_Form
+    student_form = StudentSignUp_Form
 
-    return render (request, "main/student_signup.html", context={"form":form})
+    return render (request, "main/student_signup.html", context={"user_form":user_form,"student_form":student_form})
     #return render(request = request, template_name = "main/student_signup.html")
-
+    #
 # def homepage(request):
 #     return render(request = request, template_name = "main/home.html")
